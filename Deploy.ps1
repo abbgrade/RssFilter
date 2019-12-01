c3a07b01cb6b5dd17b5a9f2de58556fa8f208ae6
Steffen Kampmann <steffen.kampmann@gmail.com>
2019-12-01 14:19:58 +0100

    Added ARM template

---
+++ Deploy.ps1
@@ -0,0 +1,11 @@
+#Requires -Modules Az.Resources
+
+$ErrorActionPreference = 'Stop'
+
+Connect-AzAccount
+
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile ./azuredeploy.json
---
+++ README.md
@@ -0,0 +1,14 @@
+# RSS Filter
+
+## Deployment
+
+### Azure Resources
+
+```powershell
+Install-Module Az.Resources -Scope CurrentUser
+./Deploy.ps1
+```
+
+### Github Action
+
+https://docs.microsoft.com/en-us/azure/azure-functions/functions-how-to-github-actions
